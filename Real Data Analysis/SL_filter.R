9d8ab35e7676d884d5521035978e59fb771852d7
Jia21 <elainewen87@gmail.com>
2018-05-20 12:30:49 -0400

    Update SL_filter.R

--- Real Data Analysis/SL_filter.R
+++ Real Data Analysis/SL_filter.R
@@ -17,20 +17,14 @@ geno1_filter <- as.matrix(geno1_filter);
 pheno_stand <- scale(pheno1);
 geno_stand <- scale(geno1_filter);
 ptm <- proc.time();
-
 ######Main effect-single locus:
-sig_index = which(abs(t(pheno_stand) %*% geno_stand/nrow(geno_stand)) > 0.20);
+sig_index = which(abs(t(pheno_stand) %*% geno_stand/nrow(geno_stand)) > 0.02);
 geno_stand_main = geno1_filter[,sig_index];
 geno_stand_main <- as.matrix(geno_stand_main);
 sig_main <- NULL;
 for(i in 1:ncol(geno_stand_main)){
   sole_geno = geno_stand_main[,i,drop=FALSE];
-  model = lm(formula = pheno1 ~ sole_geno);
-  modelsum = summary(model);
-  modelCoeffs <- modelsum$coefficients;
-  p_value =  modelCoeffs["sole_geno", "Pr(>|t|)"];
-  beta = modelCoeffs["sole_geno", "Estimate"];
-  sig_main=rbind(sig_main,matrix(c(colnames(sole_geno),beta,p_value),ncol = 3));
+  sig_main=rbind(sig_main,matrix(c(colnames(sole_geno)),ncol = 1));
 }
 
 sig_epi_sum <- NULL;
@@ -45,26 +39,17 @@ for(k in 1:(ncol(geno1_filter)-1)){
   pseudo_mat = as.matrix(pseudo_mat);
   pseudo_mat_stand = scale(pseudo_mat);
  
-  epi_index = which(abs(t(pheno_stand) %*% pseudo_mat_stand/nrow(pseudo_mat_stand)) > 0.15);
+  epi_index = which(abs(t(pheno_stand) %*% pseudo_mat_stand/nrow(pseudo_mat_stand)) > 0.05);
   if(dim(as.matrix(epi_index))[[1]]!=0){
     pseudo_mat_stand_epi = pseudo_mat[,epi_index,drop=FALSE];
     sig_epi <- NULL;
     for(i in 1:ncol(pseudo_mat_stand_epi)){
-      sole_geno = pseudo_mat_stand_epi[,i,drop=FALSE];
-      model = lm(formula = pheno1 ~ sole_geno);
-      modelsum = summary(model);
-      modelCoeffs <- modelsum$coefficients;
-      p_value =  modelCoeffs["sole_geno", "Pr(>|t|)"];
-      beta = modelCoeffs["sole_geno", "Estimate"];
-      sig_epi=rbind(sig_epi,matrix(c(colnames(sole_geno),beta,p_value),ncol = 3));
+       sole_geno = pseudo_mat_stand_epi[,i,drop=FALSE];
+       sig_epi=rbind(sig_epi,matrix(c(colnames(sole_geno)),ncol = 1));
     }
     sig_epi_sum <- rbind(sig_epi_sum,sig_epi);
   }
 }
 proc.time()-ptm;
-
-res <- rbind(sig_main,sig_epi_sum);
-fdr = matrix(p.adjust(res[,3,drop=FALSE], method = "fdr"),ncol = 1);
-fdr = format(fdr,sci=FALSE);
-Blup <- cbind(res,fdr);
+Blup <- rbind(sig_main,sig_epi_sum);
 write.table(Blup,"Blup_real_res",quote=F,sep="\t",col.names = F,row.names = T)
